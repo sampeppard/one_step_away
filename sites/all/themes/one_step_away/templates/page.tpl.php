dc8d5638a62f9d0c9d3673cd6e54c8a1873180ba
sp <sampeppard@gmail.com>
2016-12-23 14:27:49 -0500

    add pay pal module

--- sites/all/themes/one_step_away/templates/page.tpl.php
+++ sites/all/themes/one_step_away/templates/page.tpl.php
@@ -15,39 +15,55 @@
   </div>
 </header>
 
-<div class="container">
-  <div class="row">
-    <div class="header clearfix">
-      <div class="logo-div col-xs-12 col-sm-3">
-        <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-          </a>
-        <?php endif; ?>
-      </div>
-      <div class="main-menu col-xs-12 col-sm-9">
-        <?php print render($page['main_menu']); ?>
-      </div>
-    </div>
-  </div>
 
-  <div class="content-container-div clearfix">
-    <?php if ($messages): ?>
-      <div id="messages">
-        <div class="section clearfix">
-          <?php print $messages; ?>
+<div class="cover-image">
+    <div class="container">
+      <div class="row">
+        <div class="header clearfix">
+          <div class="logo-div col-xs-12 col-sm-3">
+            <?php if ($logo): ?>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+                <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+              </a>
+            <?php endif; ?>
+          </div>
+          <div class="main-menu col-xs-12 col-sm-9">
+            <?php print render($page['main_menu']); ?>
+          </div>
         </div>
       </div>
-    <?php endif; ?>
+    </div>
+</div>
 
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
 
-    <h1><?php print $title; ?></h1>
 
-    <?php print render($page['content']); ?>
-  </div>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-9 content-container-div clearfix">
+      <?php if ($messages): ?>
+        <div id="messages">
+          <div class="section clearfix">
+            <?php print $messages; ?>
+          </div>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($breadcrumb): ?>
+        <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+      <?php endif; ?>
+
+      <h1><?php print $title; ?></h1>
+
+      <?php print render($page['content']); ?>
+
+    </div>
+
+    <?php if ($page['sidebar_first']): ?>
+     <div id="sidebar" class="col-xs-12 col-sm-3">
+       <?php print render($page['sidebar_first']); ?>
+     </div>
+   <?php endif; ?>
+ </div>
 </div>
 
 <footer>
